03ece9ced17c2991a4d4e488a9e815b4f3b19f03
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-03-28 14:31:45 -0700

    ups

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using Lvl3Week3Day2_BlogBackend.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using task_hub_backend.Models;
 using task_hub_backend.Models.DTO;
@@ -5,49 +6,55 @@ using task_hub_backend.Services;
 
 namespace task_hub_backend.Controllers;
 
-    [ApiController]
-    [Route("[controller]")]
-    public class UserController : ControllerBase
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly UserService _data;
+
+    public UserController(UserService data)
     {
-        private readonly UserService _data;
-
-        public UserController(UserService data)
-        {
-            _data = data;
-        }
-
-        [HttpPost]
-        [Route("Login")]
-        public IActionResult Login([FromBody] LoginDTO User)
-        {
-            return _data.Login(User);
-        }
-
-        [HttpPost]
-        [Route("AddUser")]
-        public bool AddUser(CreateAccountDTO UserToAdd)
-        {
-            return _data.AddUser(UserToAdd);
-        }
-
-        [HttpPut]
-        [Route("UpdateUser")]
-        public bool UpdateUser(UserModel userToUpdate)
-        {
-            return _data.UpdateUser(userToUpdate);
-        }
-
-        [HttpPut]
-        [Route("UpdateUser/{id}/{username}")]
-        public bool UpdateUsername(int id, string username)
-        {
-            return _data.UpdateUsername(id, username);
-        }
-
-        [HttpDelete]
-        [Route("DeleteUser/{userToDelete}")]
-        public bool DeleteUser(string userToDelete)
-        {
-            return _data.DeleteUser(userToDelete);
-        }
+        _data = data;
     }
+
+    [HttpPost]
+    [Route("Login")]
+    public IActionResult Login([FromBody] LoginDTO User)
+    {
+        return _data.Login(User);
+    }
+
+    [HttpPost]
+    [Route("AddUser")]
+    public bool AddUser(CreateAccountDTO UserToAdd)
+    {
+        return _data.AddUser(UserToAdd);
+    }
+
+    [HttpPut]
+    [Route("UpdateUser")]
+    public bool UpdateUser(UserModel userToUpdate)
+    {
+        return _data.UpdateUser(userToUpdate);
+    }
+
+    [HttpPut]
+    [Route("UpdateUser/{id}/{username}")]
+    public bool UpdateUsername(int id, string username)
+    {
+        return _data.UpdateUsername(id, username);
+    }
+
+    [HttpDelete]
+    [Route("DeleteUser/{userToDelete}")]
+    public bool DeleteUser(string userToDelete)
+    {
+        return _data.DeleteUser(userToDelete);
+    }
+    [HttpGet]
+    [Route("GetUserByUsername/{username}")]
+    public UserIdDTO GetUserByUsername(string username)
+    {
+        return _data.GetUserIdDTOByUsername(username);
+    }
+}
---
+++ Models/DTO/UserIdDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lvl3Week3Day2_BlogBackend.Models.DTO
+{
+    public class UserIdDTO
+    {
+        public int UserId { get; set; }
+        public string PublishedName { get; set; }
+    }
+}
