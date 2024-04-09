76a17de4ebdcba6fd2c4e8c5df21b915aea16dcc
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-04-09 11:03:54 -0700

    models added

---
+++ Models/RelationModel.cs
@@ -0,0 +1,7 @@
+namespace task_hub_backend.Models;
+public class RelationModel
+{
+    public int UserProjectID { get; set; }
+    public int ProjectID2 { get; set; }
+    public int UserID { get; set; }
+}
---
+++ Models/TaskModel.cs
@@ -0,0 +1,15 @@
+
+namespace task_hub_backend.Models;
+public class TaskModel
+{
+    public int TaskID { get; set; }
+    public int ProjectID3 { get; set; }
+    public string? TaskName { get; set; }
+    public string? TaskDescription { get; set; }
+    public int UserID { get; set; }
+    public string? DueDate { get; set; }
+    public string? TaskDuration { get; set; }
+    public string? Priority { get; set; }
+    public string? Status { get; set; }
+    public bool IsDeleted { get; set; }
+}
